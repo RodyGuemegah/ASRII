7b83f9daeedeb3a1f37174df2c4fc704000e6f58
david <david.valeriuss@gmail.com>
2024-05-22 16:22:21 +0200

    Push ajout dossier

--- pageHtml/enseignant.html.php
+++ pageHtml/enseignant.html.php
@@ -6,22 +6,22 @@ headerASRI(true, 1);
 
 <div class="container mt-5">
     <!-- Première ligne de boîtes -->
-    <div class="row bg-danger m-auto mb-5" style="width: 90%;">
-        <div class="bg-info col-6 p-4 text-center text-white">
+    <div class="row m-auto mb-5" style="width: 90%;">
+        <div class="col-6 p-4 text-center text-white">
             <a class="border bg-secondary d-block text-white p-2 noLink" href="../fonctionPHP/enseignant.php">Ajouter / Supprimer un support de cours</a>
         </div>
-        <div class="bg-success col-6 p-4 text-center text-white">
-            <a class="border w-50 bg-secondary d-block text-white p-2 noLink" href="#">Box 3</a>
+        <div class=" col-6 p-4 text-center text-white">
+            <a class="border bg-secondary d-block text-white p-2 noLink" href="#">Ajouter / Supprimer une note</a>
         </div>
     </div>
 
     <!-- Deuxième ligne de boîtes -->
-    <div class="row bg-danger m-auto" style="width: 90%;">
-        <div class="bg-info col-6 p-4 text-center text-white">
-            <a class="border w-50 bg-secondary d-block text-white p-2 noLink" href="#">Box 3</a>
+    <div class="row m-auto" style="width: 90%;">
+        <div class="col-6 p-4 text-center text-white">
+            <a class="border bg-secondary d-block text-white p-2 noLink" href="#">Box 3</a>
         </div>
-        <div class="bg-success col-6 p-4 text-center text-white">
-            <a class="border w-50 bg-secondary d-block text-white p-2 noLink" href="#">Box 3</a>
+        <div class="col-6 p-4 text-center text-white">
+            <a class="border bg-secondary d-block text-white p-2 noLink" href="#">Box 3</a>
         </div>
     </div>
 </div>
